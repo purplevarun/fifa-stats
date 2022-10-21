0e1d8c558598103359460eb0d308de3057cae9da
purplevarun <purplevarun@gmail.com>
2022-10-22 00:14:52 +0530

    add frontend boilerplate code

---
+++ frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const app = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(app);
+root.render(<App />);
